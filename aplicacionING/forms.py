e07f459353fe3e420f949d3a251681d47a35b2fd
Dani <deat_1106@outlook.com>
2024-10-25 00:00:14 -0700

    mejoras visuales

--- aplicacionING/forms.py
+++ aplicacionING/forms.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import User
 
 
 class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(label='Usuario')
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
     error_messages = {
         'invalid_login': _(
             "Usuario o contraseña incorrectos. Por favor, intenta nuevamente."
@@ -15,11 +17,16 @@ class CustomAuthenticationForm(AuthenticationForm):
     }
 
 class ProjectFolderForm(forms.ModelForm):
+    name = forms.CharField(label='Nombre')
+    description = forms.CharField(label='Descripcion')
     class Meta:
         model = ProjectFolder
         fields = ['name', 'description']
 
 class ActivityFolderForm(forms.ModelForm):
+    name = forms.CharField(label='Nombre')
+    description = forms.CharField(label='Descripcion')
+    due_date= forms.CharField(label="Fecha de Termino")
     class Meta:
         model = ActivityFolder
         fields = ['name', 'description', 'due_date']
@@ -28,7 +35,8 @@ class ActivityFolderForm(forms.ModelForm):
         }
 
 class RegistroForm(forms.ModelForm):
-
+    username = forms.CharField(label='Nombre de usuario')  # Etiqueta personalizada
+    email = forms.EmailField(label='Correo electrónico')  # Etiqueta personalizada
     password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Confirma tu contraseña', widget=forms.PasswordInput)
 
@@ -52,6 +60,8 @@ class RegistroForm(forms.ModelForm):
 
     
 class ProjectForm(forms.ModelForm):
-        class Meta:
+    name = forms.CharField(label='Nombre')
+    description = forms.CharField(label='Descripcion')
+    class Meta:
             model = ProjectFolder
             fields = ['name', 'description']
